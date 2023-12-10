cee5828940b1704359d42ae8ad6964a4e32af4d0
elrinor <ru.elric@gmail.com>
2023-12-10 18:51:16 +0000

    Actually build for the target arch

--- scripts/build_test_all.sh
+++ scripts/build_test_all.sh
@@ -2,7 +2,7 @@
 
 # Parse args.
 if [[ "$1" == "-h" ]]; then
-    echo "Usage: $(basename "$0") [-jTHREADS] BUILD_PLATFORM REPO_DIR"
+    echo "Usage: $(basename "$0") [-jTHREADS] BUILD_PLATFORM BUILD_ARCH REPO_DIR"
     exit 1
 fi
 
@@ -12,16 +12,77 @@ if [[ "$1" == -j* ]]; then
     shift 1
 fi
 
-if [[ "$#" != 2 ]]; then
+if [[ "$#" != 3 ]]; then
     echo "Two arguments required. Use -h for help."
     exit 1
 fi
 
 BUILD_PLATFORM="$1"
-REPO_DIR="$2"
+BUILD_ARCH="$2"
+REPO_DIR="$3"
+
+# Check ANDROID_NDK.
+if [[ "$BUILD_PLATFORM" == "android" && "$ANDROID_NDK" == "" ]]; then
+    echo "Please provide path to your NDK via ANDROID_NDK environment variable!"
+    exit 1
+fi
 
 # Echo on, fail on errors, fail on undefined var usage, fail on pipeline failure.
 set -euxo pipefail
+
+# Prepare cmake flags.
+ADDITIONAL_CMAKE_ARGS=()
+RUN_TESTS=true
+if [[ "$BUILD_PLATFORM" == "darwin" ]]; then
+    ADDITIONAL_CMAKE_ARGS+=(
+        "-DCMAKE_OSX_ARCHITECTURES=$BUILD_ARCH"
+    )
+    if [[ "$BUILD_ARCH" == "arm64" ]]; then
+        # This is a cross-compile, can't run tests.
+        RUN_TESTS=false
+    fi
+elif [[ "$BUILD_PLATFORM" == "windows" ]]; then
+    ADDITIONAL_CMAKE_ARGS+=(
+        "-GNinja" # Use Ninja on windows. Default is MSVC project files, which are multi-config, and it's a mess.
+    )
+elif [[ "$BUILD_PLATFORM" == "linux" ]]; then
+    if [[ "$BUILD_ARCH" == "x86" ]]; then
+        ADDITIONAL_CMAKE_ARGS+=(
+            "-DCMAKE_CXX_FLAGS=-m32"
+            "-DCMAKE_C_FLAGS=-m32"
+        )
+    fi
+elif [[ "$BUILD_PLATFORM" == "android" ]]; then
+    if [[ "$BUILD_ARCH" == "arm32" ]]; then
+        ANDROID_ARCH_ABI=armeabi-v7a
+    elif [[ "$BUILD_ARCH" == "arm64" ]]; then
+        ANDROID_ARCH_ABI=arm64-v8a
+    elif [[ "$BUILD_ARCH" == "x86" ]]; then
+        ANDROID_ARCH_ABI=x86
+    elif [[ "$BUILD_ARCH" == "x86_64" ]]; then
+        ANDROID_ARCH_ABI=x86_64
+    fi
+
+    ANDROID_PLATFORM_VERSION=21
+    ANDROID_TOOLCHAIN=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64
+
+    # These are derived from what gradle passes to cmake.
+    # Args with ANDROID_ prefix are processed by the toolchain file.
+    ADDITIONAL_CMAKE_ARGS+=(
+        "-DCMAKE_SYSTEM_NAME=Android"
+        "-DCMAKE_SYSTEM_VERSION=$ANDROID_PLATFORM_VERSION"
+        "-DANDROID_PLATFORM=android-$ANDROID_PLATFORM_VERSION"
+        "-DANDROID_ABI=$ANDROID_ARCH_ABI"
+        "-DCMAKE_ANDROID_ARCH_ABI=$ANDROID_ARCH_ABI"
+        "-DANDROID_NDK=$ANDROID_NDK"
+        "-DCMAKE_ANDROID_NDK=$ANDROID_NDK"
+        "-DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake"
+        "-DANDROID_STL=c++_static"
+    )
+
+    # This is a cross-compile, can't run tests.
+    RUN_TESTS=false
+fi
 
 function build_test_one() {
     local BUILD_TYPE="$1"
@@ -32,7 +93,6 @@ function build_test_one() {
 
     echo "================================================================================================"
 
-    # TODO(elric): this actually doesn't use android tools.
     cmake \
         -B "$BUILD_DIR" \
         -S "$REPO_DIR" \
@@ -40,9 +100,13 @@ function build_test_one() {
         "-DSN_FORMAT_LIB=$FORMAT_LIB" \
         "-DSN_FLOAT_LIB=$FLOAT_LIB" \
         "-DSN_TYPE_NAME_IMPL=$TYPE_NAME_IMPL" \
-        "-DSN_CHECK_STYLE=OFF"
+        "-DSN_CHECK_STYLE=OFF" \
+        "${ADDITIONAL_CMAKE_ARGS[@]}"
     cmake --build "$BUILD_DIR" $THREADS_ARG
-    cmake --build "$BUILD_DIR" --target run_all_tests
+
+    if [[ "$RUN_TESTS" == "true" ]]; then
+        cmake --build "$BUILD_DIR" --target run_all_tests
+    fi
 }
 
 for BUILD_TYPE in "Debug" "Release"
@@ -56,8 +120,8 @@ do
 
     build_test_one "$BUILD_TYPE" "build-$BUILD_TYPE-3" "fmt_bundled" "strtof" "funcsig"
 
-    # AppleClang doesn't have floating-point std::from_chars
-    if [[ "$BUILD_PLATFORM" != "darwin" ]]; then
+    # AppleClang and Android clang don't have floating-point std::from_chars
+    if [[ "$BUILD_PLATFORM" != "darwin" && "$BUILD_PLATFORM" != "android" ]]; then
         build_test_one "$BUILD_TYPE" "build-$BUILD_TYPE-3" "fmt_bundled" "from_chars" "funcsig"
     fi
 
