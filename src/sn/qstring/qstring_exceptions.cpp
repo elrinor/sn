5fc98e97c380f2081f7ce6555673d9a0d8d22948
elrinor <ru.elric@gmail.com>
2024-01-27 16:12:12 +0000

    Drop unused code.

--- src/sn/qstring/qstring_exceptions.cpp
+++ src/sn/qstring/qstring_exceptions.cpp
@@ -8,8 +8,4 @@ void throw_from_string_error(std::string_view type_name, QStringView value) {
     sn::throw_from_string_error(type_name, std::string_view(value.toUtf8()));
 }
 
-void throw_number_from_string_error(std::string_view type_name, QStringView value, std::errc error) {
-    sn::throw_number_from_string_error(type_name, std::string_view(value.toUtf8()), error);
-}
-
 } // namespace sn
--- src/sn/qstring/qstring_exceptions.h
+++ src/sn/qstring/qstring_exceptions.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <system_error>
 #include <string_view>
 
 #include <QtCore/QStringView> // NOLINT.
@@ -10,16 +9,10 @@
 namespace sn {
 
 [[noreturn]] void throw_from_string_error(std::string_view type_name, QStringView value);
-[[noreturn]] void throw_number_from_string_error(std::string_view type_name, QStringView value, std::errc error);
 
 template<class T>
 [[noreturn]] void throw_from_string_error(QStringView value) {
     throw_from_string_error(sn::type_name<T>(), value);
-}
-
-template<class T>
-[[noreturn]] void throw_number_from_string_error(QStringView value, std::errc error) {
-    throw_number_from_string_error(sn::type_name<T>(), value, error);
 }
 
 } // namespace sn
