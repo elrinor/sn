adfbfeec09a4b752c048e71c055a4a114aa56533
elrinor <ru.elric@gmail.com>
2024-01-27 15:34:56 +0000

    Better string tests

--- src/sn/string/string_ut.cpp
+++ src/sn/string/string_ut.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <limits>
+
 #include "string.h"
 
 template<class T>
@@ -46,27 +48,80 @@ TEST(string, boolean) {
     EXPECT_ANY_THROW((void) sn::from_string<bool>(""));
 }
 
+std::string prepend_zeros(int zeros, std::string_view number_string) {
+    std::string result;
+
+    if (number_string.starts_with('-')) {
+        result = "-";
+        number_string = number_string.substr(1);
+    }
+
+    result += std::string(zeros, '0');
+    result += number_string;
+    return result;
+}
+
+template<class T>
+void run_integer_tests() {
+    EXPECT_EQ(sn::to_string(static_cast<T>(0)), "0");
+    EXPECT_EQ(sn::from_string<T>("0"), 0);
+
+    EXPECT_EQ(sn::to_string(static_cast<T>(1)), "1");
+    EXPECT_EQ(sn::from_string<T>("1"), 1);
+
+    EXPECT_EQ(sn::to_string(static_cast<T>(100)), "100");
+    EXPECT_EQ(sn::from_string<T>("100"), 100);
+
+    EXPECT_EQ(sn::from_string<T>(sn::to_string(std::numeric_limits<T>::max())), std::numeric_limits<T>::max());
+    EXPECT_EQ(sn::from_string<T>(sn::to_string(std::numeric_limits<T>::min())), std::numeric_limits<T>::min());
+
+    EXPECT_EQ(sn::from_string<T>(prepend_zeros(1, sn::to_string(std::numeric_limits<T>::max()))), std::numeric_limits<T>::max());
+    EXPECT_EQ(sn::from_string<T>(prepend_zeros(1, sn::to_string(std::numeric_limits<T>::min()))), std::numeric_limits<T>::min());
+
+    EXPECT_EQ(sn::from_string<T>(prepend_zeros(100, sn::to_string(std::numeric_limits<T>::max()))), std::numeric_limits<T>::max());
+    EXPECT_EQ(sn::from_string<T>(prepend_zeros(100, sn::to_string(std::numeric_limits<T>::min()))), std::numeric_limits<T>::min());
+
+    if constexpr (sizeof(T) < sizeof(long long)) {
+        EXPECT_ANY_THROW((void) sn::from_string<T>(sn::to_string(static_cast<long long>(std::numeric_limits<T>::max()) + 1)));
+        EXPECT_ANY_THROW((void) sn::from_string<T>(sn::to_string(static_cast<long long>(std::numeric_limits<T>::min()) - 1)));
+    } else {
+        static_assert(sizeof(T) == 8);
+
+        if constexpr (std::is_unsigned_v<T>) {
+            EXPECT_ANY_THROW((void) sn::from_string<T>("-1"));
+            EXPECT_ANY_THROW((void) sn::from_string<T>("18446744073709551616")); // max unsigned long long +1
+        } else {
+            EXPECT_ANY_THROW((void) sn::from_string<T>("–9223372036854775809")); // min long long -1
+            EXPECT_ANY_THROW((void) sn::from_string<T>("9223372036854775808")); // max long long +1
+        }
+    }
+
+    EXPECT_ANY_THROW((void) sn::from_string<T>(""));
+    EXPECT_ANY_THROW((void) sn::from_string<T>(" 1"));
+    EXPECT_ANY_THROW((void) sn::from_string<T>("1 "));
+    EXPECT_ANY_THROW((void) sn::from_string<T>("+1"));
+    EXPECT_ANY_THROW((void) sn::from_string<T>("--1"));
+    EXPECT_ANY_THROW((void) sn::from_string<T>("0x1"));
+    EXPECT_ANY_THROW((void) sn::from_string<T>("0b1"));
+    EXPECT_ANY_THROW((void) sn::from_string<T>("0-0"));
+    EXPECT_ANY_THROW((void) sn::from_string<T>("0-1"));
+}
+
 TEST(string, ints) {
-    EXPECT_EQ(sn::from_string<int>(sn::to_string(0)), 0);
-    EXPECT_EQ(sn::from_string<int>(sn::to_string(INT_MAX)), INT_MAX);
-    EXPECT_EQ(sn::from_string<int>(sn::to_string(INT_MIN)), INT_MIN);
-    EXPECT_EQ(sn::from_string<unsigned int>(sn::to_string(UINT_MAX)), UINT_MAX);
-    EXPECT_EQ(sn::from_string<unsigned long long>(sn::to_string(ULLONG_MAX)), ULLONG_MAX);
-
-    EXPECT_EQ(sn::from_string<int>("0100"), 100);
-    EXPECT_EQ(sn::from_string<int>(std::string(100, '0') + sn::to_string(INT_MAX)), INT_MAX);
-
-    EXPECT_ANY_THROW((void) sn::from_string<int>(""));
-    EXPECT_ANY_THROW((void) sn::from_string<int>(" 100"));
-    EXPECT_ANY_THROW((void) sn::from_string<int>("100 "));
-    EXPECT_ANY_THROW((void) sn::from_string<int>("+100"));
-    EXPECT_ANY_THROW((void) sn::from_string<int>("0x200"));
-    EXPECT_ANY_THROW((void) sn::from_string<int>("0b100"));
+    run_integer_tests<short>();
+    run_integer_tests<unsigned short>();
+    run_integer_tests<int>();
+    run_integer_tests<unsigned int>();
+    run_integer_tests<long>();
+    run_integer_tests<unsigned long>();
+    run_integer_tests<long long>();
+    run_integer_tests<unsigned long long>();
 }
 
 TEST(string, floats) {
     EXPECT_EQ(sn::from_string<float>(sn::to_string(1.5)), 1.5);
     EXPECT_EQ(sn::to_string(1.5), "1.5");
+    EXPECT_EQ(sn::to_string(-1.5), "-1.5");
 
     EXPECT_ANY_THROW((void) sn::from_string<float>(" 1.5"));
     EXPECT_ANY_THROW((void) sn::from_string<float>("1.5 "));
