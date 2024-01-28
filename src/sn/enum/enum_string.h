fbad53e064c18a98f71f7869e23a062d1e370cac
elrinor <ru.elric@gmail.com>
2024-01-28 13:17:38 +0000

    Private API to use custom enum tables

--- src/sn/enum/enum_string.h
+++ src/sn/enum/enum_string.h
@@ -3,8 +3,8 @@
 #include <string>
 #include <string_view>
 
-#include "sn/string/string.h"
 #include "sn/core/preprocessor.h"
+#include "sn/string/string.h"
 
 #if SN_USE_STD_ENUM_HASH
 #   include "std_enum_table.h"
@@ -21,14 +21,14 @@
 
 #define _SN_APPEND_PARENS(X) X()
 
-#define _SN_DEFINE_ENUM_STRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, ATTRIBUTES, ... /* TAGS */)                          \
+#define _SN_DEFINE_ENUM_STRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, TABLE_DEFINITION_MACRO, ATTRIBUTES, ... /* TAGS */)  \
     template<class...>                                                                                                  \
     struct _enum_table_container;                                                                                       \
                                                                                                                         \
     template<>                                                                                                          \
     struct _enum_table_container<ENUM __VA_OPT__(,) __VA_ARGS__> {                                                      \
         static constexpr auto reflection = sn::reflect_enum<ENUM>(__VA_OPT__(SN_PP_TUPLE_FOR_EACH(_SN_APPEND_PARENS, (__VA_ARGS__)))); \
-        _SN_DEFINE_ENUM_STRING_TABLE(value, ENUM, CASE_SENSITIVITY, reflection)                                         \
+        TABLE_DEFINITION_MACRO(value, ENUM, CASE_SENSITIVITY, reflection)                                               \
     };                                                                                                                  \
                                                                                                                         \
     [[nodiscard]] ATTRIBUTES bool try_to_string(const ENUM &src, std::string *dst __VA_OPT__(,) __VA_ARGS__) noexcept { \
@@ -48,10 +48,10 @@
     }
 
 #define SN_DEFINE_ENUM_STRING_FUNCTIONS(ENUM, CASE_SENSITIVITY, ... /* TAGS */)                                         \
-    _SN_DEFINE_ENUM_STRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, [[]] __VA_OPT__(,) __VA_ARGS__)
+    _SN_DEFINE_ENUM_STRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, _SN_DEFINE_ENUM_STRING_TABLE, [[]] __VA_OPT__(,) __VA_ARGS__)
 
 #define SN_DEFINE_INLINE_ENUM_STRING_FUNCTIONS(ENUM, CASE_SENSITIVITY, ... /* TAGS */)                                  \
-    _SN_DEFINE_ENUM_STRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, inline __VA_OPT__(,) __VA_ARGS__)
+    _SN_DEFINE_ENUM_STRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, _SN_DEFINE_ENUM_STRING_TABLE, inline __VA_OPT__(,) __VA_ARGS__)
 
 #define SN_DEFINE_STATIC_ENUM_STRING_FUNCTIONS(ENUM, CASE_SENSITIVITY, ... /* TAGS */)                                  \
-    _SN_DEFINE_ENUM_STRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, static __VA_OPT__(,) __VA_ARGS__)
+    _SN_DEFINE_ENUM_STRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, _SN_DEFINE_ENUM_STRING_TABLE, static __VA_OPT__(,) __VA_ARGS__)
--- src/sn/qenum/enum_qstring.h
+++ src/sn/qenum/enum_qstring.h
@@ -3,19 +3,20 @@
 #include <QtCore/QString>
 #include <QtCore/QStringView>
 
-#include "sn/qstring/qstring.h"
+#include "sn/core/preprocessor.h"
 #include "sn/enum/enum_reflection.h"
+#include "sn/qstring/qstring.h"
 
 #include "std_qstring_enum_table.h"
 
-#define _SN_DEFINE_ENUM_QSTRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, ATTRIBUTES, ... /* TAGS */)                         \
+#define _SN_DEFINE_ENUM_QSTRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, TABLE_DEFINITION_MACRO, ATTRIBUTES, ... /* TAGS */) \
     template<class...>                                                                                                  \
     struct _enum_table_container;                                                                                       \
                                                                                                                         \
     template<>                                                                                                          \
     struct _enum_table_container<ENUM __VA_OPT__(,) __VA_ARGS__> {                                                      \
-        static constexpr auto reflection = sn::reflect_enum<ENUM __VA_OPT__(,) __VA_ARGS__>();                          \
-        _SN_DEFINE_ENUM_QSTRING_TABLE(value, ENUM, CASE_SENSITIVITY, reflection)                                        \
+        static constexpr auto reflection = sn::reflect_enum<ENUM>(__VA_OPT__(SN_PP_TUPLE_FOR_EACH(_SN_APPEND_PARENS, (__VA_ARGS__)))); \
+        TABLE_DEFINITION_MACRO(value, ENUM, CASE_SENSITIVITY, reflection)                                               \
     };                                                                                                                  \
                                                                                                                         \
     [[nodiscard]] ATTRIBUTES bool try_to_qstring(const ENUM &src, QString *dst __VA_OPT__(,) __VA_ARGS__) noexcept {    \
@@ -35,10 +36,10 @@
     }
 
 #define SN_DEFINE_ENUM_QSTRING_FUNCTIONS(ENUM, CASE_SENSITIVITY, ... /* TAGS */)                                        \
-    _SN_DEFINE_ENUM_QSTRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, [[]] __VA_OPT__(,) __VA_ARGS__)
+    _SN_DEFINE_ENUM_QSTRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, _SN_DEFINE_ENUM_QSTRING_TABLE, [[]] __VA_OPT__(,) __VA_ARGS__)
 
 #define SN_DEFINE_INLINE_ENUM_QSTRING_FUNCTIONS(ENUM, CASE_SENSITIVITY, ... /* TAGS */)                                 \
-    _SN_DEFINE_ENUM_QSTRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, inline __VA_OPT__(,) __VA_ARGS__)
+    _SN_DEFINE_ENUM_QSTRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, _SN_DEFINE_ENUM_QSTRING_TABLE, inline __VA_OPT__(,) __VA_ARGS__)
 
 #define SN_DEFINE_STATIC_ENUM_QSTRING_FUNCTIONS(ENUM, CASE_SENSITIVITY, ... /* TAGS */)                                 \
-    _SN_DEFINE_ENUM_QSTRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, static __VA_OPT__(,) __VA_ARGS__)
+    _SN_DEFINE_ENUM_QSTRING_FUNCTIONS_I(ENUM, CASE_SENSITIVITY, _SN_DEFINE_ENUM_QSTRING_TABLE, static __VA_OPT__(,) __VA_ARGS__)
